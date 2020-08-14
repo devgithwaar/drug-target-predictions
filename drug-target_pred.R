7d9b7816e0047b6853b5bfd6c41879e98527ddd6
van der git <8655745+devgithwaar@users.noreply.github.com>
2020-08-14 16:27:48 +0800

    Update drug-target_pred.R
    added references

--- drug-target_pred.R
+++ drug-target_pred.R
@@ -1,1 +1,50 @@
-## predicting drug-target interactions via R package "BioMedR"
+# Predicting drug-target interactions via R package "BioMedR"
+# Author: Hui-Heng Lin, PhD . On 14th Aug.2020
+
+install.packages() 
+
+
+
+
+
+# References
+""" > citation("caret")
+
+在出版物中使用程序包时引用‘caret’:
+
+  Max Kuhn (2020). caret: Classification and
+  Regression Training. R package version
+  6.0-86.
+  https://CRAN.R-project.org/package=caret
+
+A BibTeX entry for LaTeX users is
+
+  @Manual{,
+    title = {caret: Classification and Regression Training},
+    author = {Max Kuhn},
+    year = {2020},
+    note = {R package version 6.0-86},
+    url = {https://CRAN.R-project.org/package=caret},
+  }
+
+> citation("BioMedR")
+
+在出版物中使用程序包时引用‘BioMedR’:
+
+  Min-feng Zhu, Jie Dong and Dong-sheng Cao
+  (2019). BioMedR: Generating Various
+  Molecular Representations for Chemicals,
+  Proteins, DNAs, RNAs and Their
+  Interactions. R package version 1.2.1.
+  https://CRAN.R-project.org/package=BioMedR
+
+A BibTeX entry for LaTeX users is
+
+  @Manual{,
+    title = {BioMedR: Generating Various Molecular Representations for Chemicals,
+Proteins, DNAs, RNAs and Their Interactions},
+    author = {Min-feng Zhu and Jie Dong and Dong-sheng Cao},
+    year = {2019},
+    note = {R package version 1.2.1},
+    url = {https://CRAN.R-project.org/package=BioMedR},
+  } """
