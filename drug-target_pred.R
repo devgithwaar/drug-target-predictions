409c635b2621e48a18c0497be9d2591ade163bf2
van der git <8655745+devgithwaar@users.noreply.github.com>
2020-08-16 15:45:43 +0800

    Update drug-target_pred.R

--- drug-target_pred.R
+++ drug-target_pred.R
@@ -12,23 +12,75 @@ protid=unique(gpcr[,1])
 protid=unique(gpcr[,2])
 protseq=BMgetProtSeqKEGG(protid, parallel=5) # information retrieval from remote database
 
-                
-                
-"""
-Alternatively, below could be used if your network connection is not good
-
-proseq=readFASTA(system.file('vignettedata/GPCR_seq.fasta', package = 'BioMedR'))
-drugseq=as.vector(read.table(system.file('vignettedata/GPCR_smi.txt', package = 'BioMedR'), col.names = 'SMILES'))
-
-"""
-                
 drugseq=c() # a NULL
 for (id in 1:length(drugid)){drugseq[id]=BMgetDrugSmiKEGG(drugid[id])}
 
-# alternatively, if ....
+
+"""
+Alternatively, below could be used if your network connection is not good
+
+proseq = readFASTA(system.file('vignettedata/GPCR_seq.fasta', package = 'BioMedR'))
+
+drugseq = as.vector(read.table(system.file('vignettedata/GPCR_smi.txt', package = 'BioMedR'), col.names = 'SMILES'))
+
+"""                
 
 
+x0.prot=cbind(t(sapply(unlist(protseq), extrProtMoreauBroto)), t(sapply(unlist(protseq), extrProtCTDC)))
 
+x0.drug=cbind(extrDrugGraphComplete(readMolFromSmi(textConnection(drugseq))), extrDrugPubChemComplete(readMolFromSmi(textConnection(drugseq)))) # functions failed                
+                
+                
+
+x.prot=matrix(NA, nrow = nrow(gpcr), ncol=ncol(x0.prot))
+x.drug=matrix(NA, nrow = nrow(gpcr), ncol = ncol(x0.drug))
+for(i in 1:nrow(gpcr)) x.prot[i,] = x0.prot[which(gpcr[,1][i] == protid),]
+for(i in 1:nrow(gpcr)) x.drug[i,]=x0.drug(gpcr[,2][i] == drugid)
+
+
+y=as.factor(c(rep('1',nrow(gpcr)/2), rep('0', nrow(gpcr/2)))) # create label set
+
+x=getCPI(x.prot, x.drug, type='combine') # generate drug-target interaction descriptors (combined descriptor matrix) using getCPI(). 
+colnames(x) =paste('CCI', 1:dim(x)[2],sep='_')
+
+require(caret)
+x=x[,-nearZeroVar(x)]
+
+# training set split
+set.seed(20180808)
+split_index=createDataPartition(y,p=0.75, list=F)
+
+train_x=x[split_index,]
+train_y=y[split_index]
+test_x=x[-split_index,]
+test_y=y[-split_index]
+
+
+require(randomForest)
+cv_result=rf.cv(train_x,train_y, cv.fold=5, type='classification', tree=500,mtry=30) # five-fold cross validation with random forest classifier
+
+# train random forest classifier
+rf.fit=randomForest(x=train_x, y=train_y, ntree=500, mtry=30, importance=TRUE)
+
+# predict on the training set (for demonstration purpose only) and plot ROC curve
+# predict on the test set (in fact the training set)
+pre_res=predict(rf.fit,newdata=test_x, type='prob')[,2]
+
+require(pROC) #  plot the Cross validation result and test result
+require(RColorBrewer)
+pal=brewer.pal(3, 'Set1')
+opar<-par(no.readonly = TRUE)
+par(mfrow=c(1,2))
+plot.roc(train_y, cv_results$prob[,1], col=pal[2], grid=TRUE, print.auc=TRUE, main=' Cross Validation')
+plotroc.(test_y, pre_res, col=pal[1], grid=T, print.auc=T, main='prediction')
+par(opar)
+
+                
+                
+                
+                
+                
+                
 
 # Debugging and modifications of above codes by Hui-Heng Lin
 
